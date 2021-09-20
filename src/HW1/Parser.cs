9cad9157ef99369477e7605721d0275c73af1fa2
Kirill S <kirill-samsonov@bk.ru>
2021-09-20 12:05:45 +0300

    completed program

---
+++ src/HW1/Parser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace HW1
+{
+    public class Parser
+    {
+        private static char[] operations = {'+', '-', '*', '/', ':'};
+        public static int TryParse(string[] args, out int num1, out char operation, out int num2)
+        {
+            var isFirstArgInt = int.TryParse(args[0], out num1);
+            var isOperationChar = char.TryParse(args[1], out operation);
+            var isSecondArgInt = int.TryParse(args[2], out num2);
+
+            if (!(isFirstArgInt && isSecondArgInt))
+            {
+                Console.WriteLine($"{args[0]}{args[1]}{args[2]} are not a valid arguments");
+                return 1;
+            }
+            
+            if (!(isOperationChar && operations.Contains(operation)))
+            {
+                Console.WriteLine($"{args[1]} is not a supported operation");
+                return 2;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ src/HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var parseResult = Parser.TryParse(args, out var val1, out var operation, out var val2);
+            if (parseResult != 0)
+                return parseResult;
+
+            var result = Calculator.Calculate(val1, operation, val2);
+            Console.WriteLine(result);
+            return 0;
+        }
+    }
+}
