aa2a3c7b590ab5263752d337d7c1177f1de06c47
Ilshat Mukhamediarov <61285419+IlshatM@users.noreply.github.com>
2021-10-31 10:14:23 +0300

    Revert "2k 374 (#5)"
    This reverts commit a110818fa4386441b9b3c638f19d050a9857e12a.

--- src/HW5/FSharpCalculator/MaybeBuilder.fs
+++
@@ -1,8 +0,0 @@
-﻿module FSharpCalculator.MaybeBuilder
-
-type MaybeBuilder() =
-    member this.Bind(x, f)=
-        match x with
-        | Ok x -> f x
-        | Error e -> Error e
-    member this.Return x = Ok x
